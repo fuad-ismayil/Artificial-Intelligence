c47d06d0ebf295cc342a0bfa2f8fc1ebcbc55c6e
fuad-ismayil <ismayilfuad004@gmail.com>
2025-11-12 01:03:46 +0400

    Implement Swiper library and integrate Web API fetch

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,62 @@
-const faBarsBtn=document.querySelector(".fa-bars")
+AOS.init();
+const menuBtn = document.querySelector(".fa-bars")
 const mobileMenuDiv = document.querySelector(".mobile-menu")
-const faXmarkBtn=document.querySelector(".fa-xmark")
-// maus hadiseleri: click, dblclick, mouseover, mouseenter, mouseleave
-// klaviatura hadiseleri: onkeydown, onkeyup, onkeypress
-// hadise dinleyicilerinde anonim funksiyalardan istifade olunur
-faBarsBtn.addEventListener("click", function(){
-    mobileMenuDiv.classList.add("active")
-})
-faXmarkBtn.addEventListener("click", function(){
-    mobileMenuDiv.classList.remove("active")
-})
+const closeBtn = document.querySelector(".fa-xmark")
+const awardsCards = document.querySelector(".awards-in")
+
+menuBtn.addEventListener("click", openMenu)
+function openMenu() {
+    mobileMenuDiv.classList.add("active");
+}
+
+var closeMenu = () => {
+    mobileMenuDiv.classList.remove("active");
+};
+closeBtn.addEventListener("click", closeMenu);
+
+
+(async () => {
+    const awardsUrl = "./public/json/awards.json"
+    const cavab = await fetch(awardsUrl)
+    const awardsData = await cavab.json()
+    awardsData.forEach(card => {
+        awardsCards.innerHTML += `<div class="a-cards swiper-slide">
+                    <img src="${card.img}" alt="ai awards" />
+                    <h4 class="open-sans-mid text-center">${card.title}</h4>
+                    <article>${card.despcription}</article>
+                </div>`
+    })
+})();
+const brendsSwiper = new Swiper(".brends-logos", {
+    slidesPerView: 3,
+    spaceBetween: 75,
+    loop: true,
+    autoplay: {
+        delay: 1700,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+    breakpoints: {
+        0: { slidesPerView: 1 },
+        600: { slidesPerView: 3 },
+        900: { slidesPerView: 3 },
+        1200: { slidesPerView: 3 },
+    },
+});
+
+const awardsSwiper = new Swiper(".awards", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+
+    autoplay: {
+        delay: 1700,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+    breakpoints: {
+        0: { slidesPerView: 1, centeredSlides: true },     // mobil
+        768: { slidesPerView: 2, spaceBetween: 35 },   // tablet
+        1200: { slidesPerView: 4 },  // desktop
+    },
+});
